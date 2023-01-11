6ee75f1276a98f586bf91c9c1783859542200164
hom6667 <hom6667@likelion.org>
2023-01-11 22:43:15 +1000

    Update styles

--- packages/client/src/App.tsx
+++ packages/client/src/App.tsx
@@ -1,22 +1,21 @@
-import React from 'react';
-import { Routes, Route } from 'react-router-dom';
+import React from "react";
+import { Routes, Route } from "react-router-dom";
 
-import Lobby from './pages/Lobby';
-import Friends from './pages/Friends';
-import RoomList from './pages/RoomList';
-import RoomDetail from './pages/RoomDetail';
-import SeeMore from './pages/SeeMore';
-
-
+import Lobby from "./pages/Lobby";
+import Friends from "./pages/Friends";
+import RoomList from "./pages/RoomList";
+import RoomDetail from "./pages/RoomDetail";
+import SeeMore from "./pages/SeeMore";
 
 function App() {
   return (
     <Routes>
-      <Route index element={<Lobby />} /> { /* Lobby */ }
-      <Route path ="/friends" element={<Friends />} /> { /* Friends */ }
-      <Route path ="/rooms" element={<RoomList />} /> { /* RoomList */ }
-      <Route path ="/rooms/:roomId" element={<RoomDetail />} /> { /* Room Details */ }
-      <Route path ="/more" element={<SeeMore />} /> { /* See More */ }
+      <Route index element={<Lobby />} /> {/* Lobby */}
+      <Route path="/friends" element={<Friends />} /> {/* Friends */}
+      <Route path="/rooms" element={<RoomList />} /> {/* RoomList */}
+      <Route path="/rooms/:roomId" element={<RoomDetail />} />{" "}
+      {/* Room Details */}
+      <Route path="/more" element={<SeeMore />} /> {/* See More */}
     </Routes>
   );
 }
