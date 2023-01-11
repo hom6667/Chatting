40247d346f997f4111518c780d75e1ea18b73626
hom6667 <hom6667@likelion.org>
2023-01-11 19:20:31 +1000

    Finished setting session in server part

--- packages/server/src/server.ts
+++ packages/server/src/server.ts
@@ -1,9 +1,28 @@
 import express, { Application } from "express";
+import session from 'express-session';
+
 import sequelize from "./sequelize";
 import routes from "./routes";
 
+const FileStore = require("session-file-store")(session);
+
 const app: Application = express();
 
+const sessionMiddleware = session({
+    // A value to prevent arbitrary modulation of cookies. 
+    // Use this value to encrypt and save the session.
+    secret: "chatingapp", 
+    // Pre-create and save sessions as unitized before they are saved.
+    saveUninitialized: true, 
+    // one day
+    cookie: { secure: false },
+    // A value that determines whether the session is always saved (even if it is not changed).
+    // express-session documentation recommends that this value be false and sets it to true as needed.
+    resave: false, 
+    store: new FileStore(),
+});
+
+app.use(sessionMiddleware);
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
