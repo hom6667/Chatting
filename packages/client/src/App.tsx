a3a3f65c8bdac68cc11ca849605bf7981690bfab
hom6667 <hom6667@likelion.org>
2023-01-11 22:35:15 +1000

    Finished Route Settings

--- packages/client/src/App.tsx
+++ packages/client/src/App.tsx
@@ -1,9 +1,23 @@
 import React from 'react';
+import { Routes, Route } from 'react-router-dom';
+
+import Lobby from './pages/Lobby';
+import Friends from './pages/Friends';
+import RoomList from './pages/RoomList';
+import RoomDetail from './pages/RoomDetail';
+import SeeMore from './pages/SeeMore';
+
 
 
 function App() {
   return (
-    <div>Hello World</div>
+    <Routes>
+      <Route index element={<Lobby />} /> { /* Lobby */ }
+      <Route path ="/friends" element={<Friends />} /> { /* Friends */ }
+      <Route path ="/rooms" element={<RoomList />} /> { /* RoomList */ }
+      <Route path ="/rooms/:roomId" element={<RoomDetail />} /> { /* Room Details */ }
+      <Route path ="/more" element={<SeeMore />} /> { /* See More */ }
+    </Routes>
   );
 }
 
